5cea5aa6b9361eed502af6390829670eff621c9c
junpengtan <junpengtan@qq.com>
2022-08-19 16:47:52 +0800

    --tjp add 模板切分

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<div>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>@yield('title','weibo app') - Laravel 新手入门教程</title>
+  </head>
+  <body>
+  @yield('content')
+  </body>
+  </html>
+</div>
