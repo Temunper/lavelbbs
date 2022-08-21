769acb2b7e9ed71e2c6b09258cc44f70203c8a61
junpengtan <junpengtan@qq.com>
2022-08-21 11:45:10 +0800

    --tjp add 登录成功后导航逻辑

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::resource('users', 'UsersController');
 
 Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
-Route::delete('login','SessionsController@destory')->name('login');
+Route::delete('logout','SessionsController@destory')->name('logout');
