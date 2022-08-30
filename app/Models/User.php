2c45a5e59b9aca4549eb6b605b29b90be568b0c2
junpengtan <junpengtan@qq.com>
2022-08-30 12:34:05 +0800

    --tjp 关系流

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,12 +71,16 @@ class User extends Authenticatable
 
     public function followings()
     {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+        return $this->belongsToMany(User::class,
+            'followers', 'follower_id', 'user_id');
     }
 
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
@@ -95,7 +99,7 @@ class User extends Authenticatable
     }
 
     public function isFollowing($user_id){
-        return $this->followings()->contains($user_id);
+        return $this->followings->contains($user_id);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,11 @@ Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('p
 
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
